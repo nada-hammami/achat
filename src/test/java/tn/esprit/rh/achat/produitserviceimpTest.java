fe2e8451a0f89976a5e04f04b10db243af7aba34
dhia trabelsi <dhiaeddine.trabelsi@esprit.tn>
2023-11-12 01:44:08 +0100

    corrc

--- src/test/java/tn/esprit/rh/achat/produitserviceimpTest.java
+++ src/test/java/tn/esprit/rh/achat/produitserviceimpTest.java
@@ -7,12 +7,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import tn.esprit.rh.achat.entities.CategorieProduit;
-import tn.esprit.rh.achat.entities.Produit;
-import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.entities.*;
 import tn.esprit.rh.achat.repositories.ProduitRepository;
 import tn.esprit.rh.achat.repositories.StockRepository;
 import tn.esprit.rh.achat.services.ProduitServiceImpl;
+import tn.esprit.rh.achat.repositories.ReglementRepository;
+import tn.esprit.rh.achat.repositories.FactureRepository;
+import tn.esprit.rh.achat.services.ReglementServiceImpl;
+
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -27,142 +29,104 @@ import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
-class produitserviceimpTest  {
+class ReglementserviceimpTest  {
     @Mock
-    ProduitRepository produitRepository;
+    ReglementRepository reglementRepository;
 
     @Mock
-    StockRepository stockRepository;
+    FactureRepository factureRepository;
 
 
     @InjectMocks
-    ProduitServiceImpl produitService;
+    ReglementServiceImpl reglementService;
 
-    private Produit produit;
+    private Reglement reglement;
 
     @BeforeEach
     void setUp() {
-        produit = new Produit();
-        produit.setIdProduit(1L);
-        produit.setCodeProduit("testprod");
-        produit.setLibelleProduit("test Product");
-        produit.setPrix(50.0f);
+        reglement = new Reglement();
+        reglement.setIdReglement(1L);
+        reglement.setPayee(true);
+        reglement.setMontantPaye(50);
+        reglement.setMontantRestant(30);
+
 
         // Initialize date properties
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         try {
-            produit.setDateCreation(dateFormat.parse("2023-10-24"));
-            produit.setDateDerniereModification(dateFormat.parse("2023-10-24"));
+            reglement.setDateReglement(dateFormat.parse("2023-10-24"));
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        Stock stock = new Stock();
-        stock.setIdStock(2L);
-        stock.setLibelleStock("test Stock");
-        stock.setQte(100);
-        stock.setQteMin(10);
+        Facture facture = new Facture();
+        facture.setIdFacture(2L);
+        facture.setMontantFacture(50);
+        facture.setMontantRemise(100);
+        facture.setArchivee(false);
 
-        CategorieProduit categorieProduit = new CategorieProduit();
-        categorieProduit.setIdCategorieProduit(3L);
-        categorieProduit.setCodeCategorie("testcat");
-        categorieProduit.setLibelleCategorie("test Category");
+        try {
+            facture.setDateCreationFacture(dateFormat.parse("2023-10-24"));
+            facture.setDateDerniereModificationFacture(dateFormat.parse("2023-10-24"));
 
-        produit.setStock(stock);
-        produit.setCategorieProduit(categorieProduit);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
 
     @Test
-    void retrieveAllProduitsTest() {
+    void retrieveAllreglementTest() {
         // Create a list using ArrayList
-        List<Produit> produitsList = new ArrayList<>();
-        produitsList.add(produit);
+        List<Reglement> reglementlist = new ArrayList<>();
+        reglementlist.add(reglement);
 
         // Mock the behavior of produitRepository.findAll()
-        when(produitRepository.findAll()).thenReturn(produitsList);
+        when(reglementRepository.findAll()).thenReturn(reglementlist);
 
         // Call the method under test
-        List<Produit> retrievedProduitsList = produitService.retrieveAllProduits();
+        List<Reglement> retrievedProduitsList = reglementService.retrieveAllReglements();
 
         // Use assertThat(actual).hasSize(expected)
         assertThat(retrievedProduitsList).hasSize(1);
-        verify(produitRepository).findAll();
+        verify(reglementRepository).findAll();
     }
 
     @Test
     void addProduitTest() {
         // Mock the behavior of produitRepository.save()
-        when(produitRepository.save(Mockito.any(Produit.class))).thenReturn(produit);
+        when(reglementRepository.save(Mockito.any(Reglement.class))).thenReturn(reglement);
 
         // Call the method under test
-        Produit savedProduit = produitService.addProduit(produit);
+        Reglement savedReglement = reglementService.addReglement(reglement);
 
         // Assertions
-        assertThat(savedProduit).isNotNull();
-        verify(produitRepository).save(Mockito.any(Produit.class));
+        assertThat(savedReglement).isNotNull();
+        verify(reglementRepository).save(Mockito.any(Reglement.class));
     }
 
-    @Test
-    void deleteProduitTest() {
-        Long produitId = 1L;
-        // Mock the behavior of produitRepository.deleteById()
-        doNothing().when(produitRepository).deleteById(produitId);
 
-        // Call the method under test
-        produitService.deleteProduit(produitId);
 
-        // Verify that deleteById was called once
-        verify(produitRepository, times(1)).deleteById(produitId);
-    }
+
 
     @Test
-    void updateProduitTest() {
-        // Mock the behavior of produitRepository.save()
-        when(produitRepository.save(produit)).thenReturn(produit);
-
-        // Modify some properties of the produit
-        produit.setLibelleProduit("Updated Product Name");
-
-        // Call the method under test
-        Produit updatedProduit = produitService.updateProduit(produit);
-
-        // Assertions
-        assertThat(updatedProduit).isNotNull();
-        assertThat(updatedProduit.getLibelleProduit()).isEqualTo("Updated Product Name");
-        verify(produitRepository).save(produit);
-    }
-
-    @Test
-    void retrieveProduitTest() {
-        Long produitId = 1L;
+    void retrieveReglementTest() {
+        Long reglementId = 1L;
         // Mock the behavior of produitRepository.findById()
-        when(produitRepository.findById(produitId)).thenReturn(Optional.of(produit));
+        when(reglementRepository.findById(reglementId)).thenReturn(Optional.of(reglement));
 
         // Call the method under test
-        Produit retrievedProduit = produitService.retrieveProduit(produitId);
+        Reglement retrievedreglement = reglementService.retrieveReglement(reglementId);
 
         // Assertions
-        assertThat(retrievedProduit).isNotNull();
-        verify(produitRepository).findById(produitId);
+        assertThat(retrievedreglement).isNotNull();
+        verify(reglementRepository).findById(reglementId);
     }
 
-    @Test
-    void assignProduitToStockTest() {
-        Long idProduit = 1L;
-        Long idStock = 2L;
-
-        // Mock the behavior of produitRepository.findById() and stockRepository.findById()
-        when(produitRepository.findById(idProduit)).thenReturn(Optional.of(produit));
-        when(stockRepository.findById(idStock)).thenReturn(Optional.of(new Stock()));
-
-        // Call the method under test
-        produitService.assignProduitToStock(idProduit, idStock);
-
-        // Assertions or verifications as needed
-        verify(produitRepository).save(produit);
-    }
 
 
 }
